47d5cd10724b476bf543642c63c70086d4e0d713
Antonin Neumann <antonin.neumann@seznam.cz>
2021-05-12 20:13:54 +0200

    Add new genereic route for REST api

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,11 @@
 use Morgo\Mvc\AppRouter;
 
 return [
+    [
+        ['GET','POST', 'DELETE', 'PUT', 'PATCH', 'HEAD'],
+        '/rest/{version}/{controller}/{action}',
+        [new AppRouter, 'route']
+    ],
     [['GET','POST'], '/admin/{controller}/{action}/[{id:\d+}]', [new AppRouter, 'route']],
     [['GET','POST'], '/{controller}/{action}', [new AppRouter, 'route']],
 ];
